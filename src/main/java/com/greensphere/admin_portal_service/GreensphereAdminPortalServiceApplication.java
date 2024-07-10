4b446eec6978fe7eeac1067795ddaa535f50d83e
PraveenDev111 <praveent8@gmail.com>
2024-07-10 21:11:15 +0530

    Initialise Project

---
+++ src/main/java/com/greensphere/admin_portal_service/GreensphereAdminPortalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.greensphere.admin_portal_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreensphereAdminPortalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreensphereAdminPortalServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greensphere/admin_portal_service/GreensphereAdminPortalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greensphere.admin_portal_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreensphereAdminPortalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
